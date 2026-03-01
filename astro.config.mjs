c836f18b98d61ce6524eed6e4b427109895df74e
MorrisGlr <morrisglr@proton.me>
2026-03-01 15:43:02 -0500

    added yml github page deployment workflow and configured astro for github project pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://morrisglr.github.io',
+  base: '/creative',
   vite: {
     plugins: [tailwindcss()]
   }
